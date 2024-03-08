faf129c45a53191676c6717a9ad4101ba9581755
任轲 <renke@fpwis.com>
2024-03-08 09:48:42 +0800

    feat: 修改配置文件

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env sh
-###
- # @Author: rk
- # @Description: 
- # @Date: 2024-03-07 17:17:42
- # @LastEditors: rk
- # @LastEditTime: 2024-03-08 09:37:34
-### 
  
 # 忽略错误
 set -e  #有错误抛出错误
@@ -20,7 +13,7 @@ git init  #执行这些git命令
 git add -A
 git commit -m 'deploy'
  
-git push -f https://github.com/cl4u/blog.git master  #提交到这个分支
+git push -f https://github.com/cl4u/blog.git master:gh-pages  #提交到这个分支
  
 cd -
  
